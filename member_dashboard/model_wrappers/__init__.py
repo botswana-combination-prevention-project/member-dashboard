65487f2bf2ea2f7675b1f5151eb3e25084d103e0
erikvw <ew2789@gmail.com>
2017-09-02 13:08:02 +0200

    import model wrappers, update paths

---
+++ member_dashboard/model_wrappers/__init__.py
@@ -0,0 +1,2 @@
+from .model_wrappers import HouseholdMemberModelWrapper, RepresentativeEligibilityModelWrapper
+from .model_wrappers import HeadOfHouseholdEligibilityModelWrapper, HouseholdInfoModelWrapper
